022b4a9a7f00cbfb06b1a8e077ca86528114ee10
Sergio Rondon <sergio19980816@gmail.com>
2024-05-06 22:35:44 -0500

    Agregar archivos de proyecto.

---
+++ BenchMarkingTest/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Reflection;
+
+//Console.WriteLine("Hello, World!");
+
+var summary = BenchmarkRunner.Run<StringStartsWithBenchmark>();
+
+
+public class StringStartsWithBenchmark
+{
+    private const int Iterations = 100;
+    private string testString = "sample string";
+
+    [Benchmark]
+    public void StartsWithDoubleQuotes()
+    {
+        for (int i = 0; i < Iterations; i++)
+        {
+            string testString = "sample string";
+            bool startsWithS = testString.StartsWith("s");
+        }
+    }
+
+    [Benchmark]
+    public void StartsWithSingleQuotes()
+    {
+        for (int i = 0; i < Iterations; i++)
+        {
+            string testString = "sample string";
+            bool startsWithS = testString.StartsWith('s');
+        }
+    }
+}
